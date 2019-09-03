7e6a286c3145aea08e6bcce1bf1ec11f6d4a8043
Viktor Pracht <898409+VP-@users.noreply.github.com>
2019-09-03 12:00:35 +0200

    WIP: OT-348: Process metadata commands in an IMAP thread
    Made WebPush calls asynchronous. Added a new job tpye and an event to
    return the results for each of
    subscribe_webpush() and get_webpush_subscription().

--- python/src/deltachat/account.py
+++ python/src/deltachat/account.py
@@ -142,26 +142,23 @@ class Account(object):
         res = lib.dc_get_webpush_vapid_key(self._dc_context)
         return None if res == ffi.NULL else from_dc_charpointer(res)
 
-    def subscribe_webpush(self, uid, data):
+    def subscribe_webpush(self, uid, data, id):
         """ subscribe for or unsubscribe from WebPush
         :param uid: unique subscription ID, between 10 and 36 alphanumeric characters
         :param data: subscription data as a dict to subscribe or None to unsubscribe
-        :return: True on success, False otherwise
+        :param id: request number which can be used to associate the status event with this call
         """
         js = None if data is None else json.dumps(data, ensure_ascii=False,
                                                   allow_nan=False)
-        return lib.dc_subscribe_webpush(self._dc_context, uid.encode("utf8"),
-                                        js.encode("utf8")) == 1
+        lib.dc_subscribe_webpush(self._dc_context, uid.encode("utf8"),
+                                 js.encode("utf8"), id)
 
-    def get_webpush_subscription(self, uid):
+    def get_webpush_subscription(self, uid, id):
         """ returns the data of an existing WebPush subscription
         :param uid: unique ID of the subscription to retrieve
-        :return: subscription data as a dict
-                 or None if a subscription with the specified ID is not found
+        :param id: request number which can be used to associate the status event with this call
         """
-        js = lib.dc_get_webpush_subscription(self._dc_context,
-                                             uid.encode("utf8"))
-        return None if js == ffi.NULL else json.loads(from_dc_charpointer(js))
+        lib.dc_get_webpush_subscription(self._dc_context, uid.encode("utf8"), id)
 
     def get_infostring(self):
         """ return info of the configured account. """
--- src/webpush.rs
+++ src/webpush.rs
@@ -1,6 +1,6 @@
 use crate::context::*;
-use crate::error::Result;
-use crate::imap::*;
+use crate::job::*;
+use crate::param::*;
 
 #[derive(Clone)]
 pub struct WebPushConfig {
@@ -13,39 +13,22 @@ impl Default for WebPushConfig {
     }
 }
 
-const SUBSCRIPTIONS: &str = "/private/vendor/vendor.dovecot/webpush/subscriptions/";
+pub const SUBSCRIPTIONS: &str = "/private/vendor/vendor.dovecot/webpush/subscription/";
 
 impl Context {
     pub fn get_webpush_config(&self) -> Option<WebPushConfig> {
         self.inbox.read().unwrap().get_webpush_config()
     }
-    pub fn subscribe_webpush(&self, uid: &str, json: Option<&str>) -> Result<()> {
-        self.inbox.read().unwrap().set_metadata(
-            self,
-            "",
-            &[Metadata {
-                entry: [SUBSCRIPTIONS, uid].concat(),
-                value: json.map(|s| s.into()),
-            }],
-        )
+    pub fn subscribe_webpush(&self, uid: &str, json: Option<&str>, id: i32) {
+        let mut params = Params::new();
+        params.set_map(Param::Metadata,
+                       &[(&[SUBSCRIPTIONS, uid].concat(),
+                          json.map_or("", |s| s.into()))]);
+        job_add(self, Action::SetMetadata, id as libc::c_int, params, 0);
     }
-    pub fn get_webpush_subscription(&self, uid: &str) -> Result<Option<String>> {
-        let res = self.inbox.read().unwrap().get_metadata(
-            self,
-            "",
-            &[&[SUBSCRIPTIONS, uid].concat()],
-            MetadataDepth::Zero,
-            None,
-        );
-        Ok(res?.first().and_then(|m| m.value.clone()))
-    }
-    pub fn list_webpush_subscriptions(&self) -> Result<Vec<Metadata>> {
-        self.inbox.read().unwrap().get_metadata(
-            self,
-            "",
-            &[&SUBSCRIPTIONS[..SUBSCRIPTIONS.len() - 1]],
-            MetadataDepth::One,
-            None,
-        )
+    pub fn get_webpush_subscription(&self, uid: &str, id: i32) {
+        let mut params = Params::new();
+        params.set(Param::Metadata, uid);
+        job_add(self, Action::GetWebPushSubscription, id as libc::c_int, params, 0);
     }
 }
